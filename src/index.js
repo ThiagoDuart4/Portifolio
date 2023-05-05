88a17570286041f148f39cd328eae79a5876e6b8
ThiagoDuart4 <arteslima135@gmail.com>
2023-05-05 09:13:23 -0300

    Atualização,Router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+// ROUTER
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Home from "./pages/Home/Home"
+import Landing from "./pages/LandingPage/Landing"
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route  path="home" element={<Home />} />
+      <Route  path="/" index element={<Landing/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(    <App />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+

7af3ea0c134b25e308e29ddd940abdc6928b7cd6
ThiagoDuart4 <arteslima135@gmail.com>
2023-05-23 20:34:39 -0300

    DOWNLOAD CV

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import style from '../Icon/Icons.module.css'
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import {FiDownload} from 'react-icons/fi';
+import { saveAs } from 'file-saver';
+import  testes from "../../assets/curriculo.pdf"
+
+
 const Icon = () => {
-   
     return (
         <div className={style.icon}>
+            <a href="https://drive.google.com/file/d/14bEy7DG7dlOrh6QWBiZ_jViqbXArVVfn/view?usp=drivesdk " ><FiDownload/></a>
            <a href="https://www.linkedin.com/in/thiago-duarte-n/"><BsLinkedin   /></a> 
            <a href="https://github.com/ThiagoDuart4"> <BsGithub /></a>
         </div>
